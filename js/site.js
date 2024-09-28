87c79022087fc2d87a7811e662992a41450fb13e
MMTM <memtortenet@gmail.com>
2024-09-28 02:14:24 +0200

    Minden féle oldal és fájl, refakt, jobb design

--- js/site.js
+++ js/site.js
@@ -1,1 +1,1 @@
-function copyToClipboard(n,t){navigator.clipboard.writeText(n).then(function(){const n=t.outerHTML;t.outerHTML='<span id="copied-message">Copied!<\/span>';setTimeout(function(){document.getElementById("copied-message").outerHTML=n},2500)})}
+function searchFunction(){alert("Keres�s indul...")}function copyToClipboard(n,t){navigator.clipboard.writeText(n).then(function(){const n=t.outerHTML;t.outerHTML='<span id="copied-message">Copied!<\/span>';setTimeout(function(){document.getElementById("copied-message").outerHTML=n},2500)})}
